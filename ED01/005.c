d0529bd6dfd0ebf906539ecabfc2a333e734dd3e
HZP <111202256+AulusHZP@users.noreply.github.com>
2022-10-22 12:14:01 -0300

    Add files via upload

---
+++ ED01/005.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  printf("\nTriângulo");
+  int base = 3;
+  int altura = 5;
+  int altura1 = base*altura/2;
+  int terco = altura/3;
+  
+  
+  printf("\nA altura do triângulo é %i e um terco dela é %i"altura1,terco);
+  return 0;
+}
---
+++ ED01/007.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  //Exemplo0101 - v0.0. - 10/08/2022
+  //Author:Áulus Batista
+  
+  printf("\nCubo");
+  int a = 3;
+  int b = 5;
+  int c = 5;
+  int volume = a*b*c;
+  int quarto = volume/4;
+  
+  printf("\n %i - Volume",volume);
+  printf("\n %i - Um quarto do volume",quarto);
+  
+  return 0;
+}
