b3166ec708b5908c81a8d5ab791290dd91b35204
HZP <111202256+AulusHZP@users.noreply.github.com>
2022-10-22 12:13:43 -0300

    Create 001.c

---
+++ ED01/001.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  printf("\n Calcular um sexto da area de um quadrado");
+  int lado = 5;
+  int area = lado*lado;
+  int sexto = area/6;
+  printf("\nUm sexto da área é %f",sexto);
+  return 0;
+}
